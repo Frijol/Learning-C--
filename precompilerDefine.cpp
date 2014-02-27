4951669eb0096fb3de4148a828df48c5f0be0a93
Frijol <ifoundthemeaningoflife@gmail.com>
2014-02-27 13:01:12 -0500

    command line arguments

---
+++ precompilerDefine.cpp
@@ -0,0 +1,28 @@
+// illustrates use of #define with #ifdef #ifndef #else #endif
+
+#include <iostream>
+#define DemoVersion
+#define DOS_VERSION 5
+
+int main()
+{
+	std::cout << "Checking on the definitions of DemoVersion, DOS_VERSION and WINDOWS_VERSION...\n";
+	#ifdef DemoVersion
+		std::cout << "DemoVersion defined\n";
+	#else
+		std::cout << "DemoVersion not defined\n";
+	#endif
+
+	#ifndef DOS_VERSION
+		std::cout << "DOS_VERSION not defined\n";
+	#else
+		std::cout << "DOS_VERSION defined\n";
+	#endif
+
+	#ifdef WINDOWS_VERSION
+		std::cout << "WINDOWS_VERSION defined\n";
+	#else
+		std::cout << "WINDOWS_VERSION not defined\n";
+	#endif
+	return 0;
+}
