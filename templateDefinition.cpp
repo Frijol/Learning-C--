aa69871403beac8e8425f3d296ec624c76534fd7
Frijol <ifoundthemeaningoflife@gmail.com>
2014-03-02 12:37:46 -0500

    template

---
+++ templateDefinition.cpp
@@ -0,0 +1,8 @@
+// Defines a template
+template <class T>		// declare the template and the parameter
+class Array						// class being parameterized
+{
+public:
+	Array();
+	//...full class declaration
+};
